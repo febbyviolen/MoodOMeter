a05f2029bc1b1bb7bdbb3ba53a46d546424bb0ed
febbyviolen <febbyviolen@gmail.com>
2023-11-05 13:04:38 +0900

    Initial Commit

---
+++ MoodOMeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodOMeter
+//
+//  Created by Ebbyy on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
